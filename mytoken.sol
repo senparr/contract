846ceaafe38b9d35232b2703ade84a6110063ed1
senparr <trinhtung.tt@outlook.com>
2025-12-31 00:49:18 +0700

    fix decimal

--- mytoken.sol
+++ mytoken.sol
@@ -5,29 +5,26 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CappedToken is ERC20, Ownable {
-
-    // 🔒 Mint control flag (PUT THIS HERE)
     bool public mintingFinished;
-
-    // 🔢 Hard cap
     uint256 public immutable MAX_SUPPLY;
 
     constructor(
         string memory name_,
         string memory symbol_,
-        uint256 maxSupply_   // include decimals
+        uint256 maxSupplyTokens // whole tokens (no decimals)
     ) ERC20(name_, symbol_) Ownable(msg.sender) {
-        MAX_SUPPLY = maxSupply_;
+        MAX_SUPPLY = maxSupplyTokens * 10 ** decimals();
     }
 
-    // 🪙 Mint with cap + lock check (REPLACE old mint with this)
-    function mint(address to, uint256 amount) external onlyOwner {
+    function mint(address to, uint256 amountTokens) external onlyOwner {
         require(!mintingFinished, "Minting disabled");
-        require(totalSupply() + amount <= MAX_SUPPLY, "Cap exceeded");
-        _mint(to, amount);
+
+        uint256 scaled = amountTokens * 10 ** decimals();
+        require(totalSupply() + scaled <= MAX_SUPPLY, "Cap exceeded");
+
+        _mint(to, scaled);
     }
 
-    // 🔐 Permanently disable minting
     function finishMinting() external onlyOwner {
         mintingFinished = true;
     }
